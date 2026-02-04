c7dd9bdd429a3214e0d10c78e9038be52fdf4a5d
emmydapson <iloriemmanuel00@gmail.com>
2026-02-04 12:40:42 +0100

    fixed

--- src/tracking/tracking.module.ts
+++ src/tracking/tracking.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { TrackingService } from './tracking.service';
 import { TrackingController } from './tracking.controller';
-import { DigistoreController } from './digistore.controller';
+import { DigistoreController } from '../webhooks/digistore24/digistore24.controller';
 
 @Module({
   controllers: [TrackingController, DigistoreController],
