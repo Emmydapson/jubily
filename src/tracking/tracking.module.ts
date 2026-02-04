0cf93b08679bc119b32ea05357154800bcae471e
emmydapson <iloriemmanuel00@gmail.com>
2026-02-04 13:21:55 +0100

    fixed

--- src/tracking/tracking.module.ts
+++ src/tracking/tracking.module.ts
@@ -1,12 +1,15 @@
+/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { TrackingService } from './tracking.service';
 import { TrackingController } from './tracking.controller';
 import { Digistore24Controller } from '../webhooks/digistore24/digistore24.controller';
+import { Digistore24Service } from 'src/webhooks/digistore24/digistore24.service';
+
 
 @Module({
   controllers: [TrackingController, Digistore24Controller],
   providers: [TrackingService, PrismaService],
-  exports: [TrackingService],
+  exports: [TrackingService, Digistore24Service],
 })
 export class TrackingModule {}
