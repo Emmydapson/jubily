37dd68a99159c6fd57104905385d34ef1f6777e6
emmydapson <iloriemmanuel00@gmail.com>
2026-02-04 13:39:03 +0100

    fixed

--- src/tracking/tracking.module.ts
+++ src/tracking/tracking.module.ts
@@ -4,12 +4,12 @@ import { PrismaService } from '../prisma/prisma.service';
 import { TrackingService } from './tracking.service';
 import { TrackingController } from './tracking.controller';
 import { Digistore24Controller } from '../webhooks/digistore24/digistore24.controller';
-import { Digistore24Service } from 'src/webhooks/digistore24/digistore24.service';
+import { Digistore24Service } from '../webhooks/digistore24/digistore24.service';
 
 
 @Module({
   controllers: [TrackingController, Digistore24Controller],
-  providers: [TrackingService, PrismaService],
+  providers: [TrackingService, PrismaService, Digistore24Service],
   exports: [TrackingService, Digistore24Service],
 })
 export class TrackingModule {}
